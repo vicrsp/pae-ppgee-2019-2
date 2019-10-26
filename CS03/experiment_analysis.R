cf7f98553682423fd525228757dec824a90059ef
Victor Ruela <victorspruela@gmail.com>
2019-10-26 14:12:51 -0300

    juntando resultados dos algoritmos

--- CS03/experiment_analysis.R
+++ CS03/experiment_analysis.R
@@ -14,6 +14,15 @@ if (!require(dplyr, quietly = TRUE)){
 data.experiment <- read.table('experiment_raw_results.txt')
 data.experiment$ObservationLogScaled <- log(data.experiment$Observation)
 
+data.maressa <- read.csv('rcbd.config.maressa.csv')
+data.victor <- read.csv('rcbd.config.victor.csv')
+data.gilmar <- read.csv('rcbd.config.gilmar.csv')
+
+data.all.instances <- data.maressa %>% bind_rows(data.victor) %>% bind_rows(data.gilmar) %>% select(algorithm, replicate, instance, group, result)
+write.csv(data.all.instances, 'data.all.instances.csv')
+
+res.aov <- aov(result ~ algorithm + instance, data = data.all.instances)
+
 #data.experiment.grouped <- data.experiment %>% group_by(Algorithm,Instance) %>% summarise(avg = mean(Observation))
 
 ggplot(data.experiment, aes(x = ObservationLogScaled, y = Instance, fill = Algorithm)) + geom_density_ridges() +
