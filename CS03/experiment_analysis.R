dd6280b16d5668c7d36568b85a8963e5bdebcc89
Victor Ruela <victorspruela@gmail.com>
2019-10-29 19:41:23 -0300

    testes de validacao de premissas

--- CS03/experiment_analysis.R
+++ CS03/experiment_analysis.R
@@ -14,24 +14,24 @@ if (!require(dplyr, quietly = TRUE)){
 data.experiment <- read.table('experiment_raw_results.txt')
 data.experiment$ObservationLogScaled <- log(data.experiment$Observation)
 
-data.maressa <- read.csv('rcbd.config.maressa.csv')
-data.victor <- read.csv('rcbd.config.victor.csv')
-data.gilmar <- read.csv('rcbd.config.gilmar.csv')
+data.all.instances <- read.csv('data.all.instances.csv')
+data.all.instances.normalized <- data.all.instances %>% group_by(instance) %>% mutate(Max = max(result), Min = min(result), Std = sd(result), Avg = mean(result)) %>% mutate(resultMinMaxNorm = (result - Min)/(Max - Min), resultZnorm = (result - Avg)/Std)
 
-data.all.instances <- data.maressa %>% bind_rows(data.victor) %>% bind_rows(data.gilmar) %>% select(algorithm, replicate, instance, group, result)
-write.csv(data.all.instances, 'data.all.instances.csv')
+b <- 34
+random.blocks <- sample.int(150, b)
+data.block.instances <- data.all.instances.normalized %>%  filter(instance %in% random.blocks)
 
-res.aov <- aov(result ~ algorithm + instance, data = data.all.instances)
+res.aov <- aov(resultMinMaxNorm  ~ algorithm + instance, data = data.block.instances)
 
 #data.experiment.grouped <- data.experiment %>% group_by(Algorithm,Instance) %>% summarise(avg = mean(Observation))
 
-ggplot(data.experiment, aes(x = ObservationLogScaled, y = Instance, fill = Algorithm)) + geom_density_ridges() +
+ggplot(data.block.instances, aes(x = resultMinMaxNorm, y = as.factor(instance), fill = as.factor(algorithm))) + geom_density_ridges() +
   geom_density_ridges(scale = 10, size = 0.25, rel_min_height = 0.03)
 
 #ggplot(data.experiment %>% filter(Algorithm == 'algo2'), aes(x = ObservationLogScaled,  fill = Instance)) +
 #  geom_density() 
 
-ggplot(data.experiment, aes(x = Instance, 
-                            y = ObservationLogScaled, 
-                            group = Algorithm, 
-                            colour = Algorithm)) + geom_line(linetype=2) + geom_point(size=2)
+ggplot(data.block.instances, aes(x = instance, 
+                            y = resultMinMaxNorm, 
+                            group = as.factor(algorithm), 
+                            colour = as.factor(algorithm))) + geom_line(linetype=2) + geom_point(size=2)
