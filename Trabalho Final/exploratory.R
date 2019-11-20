00b17056c9edab89ba729944a78aa20c9110d3bf
Victor Ruela <victorspruela@gmail.com>
2019-11-19 21:55:43 -0300

    adicionando novos dados

--- Trabalho Final/exploratory.R
+++ Trabalho Final/exploratory.R
@@ -24,11 +24,17 @@ if (!require(multcomp, quietly = TRUE)){
 
 
 data <- read.csv('data_pre_processed_raw.csv')
-data <- data %>% filter(Mês < 11)
+data.materials <- read.csv('data_pre_processed_filt_materials.csv')
 
-data$turma = as.factor(data$Turma..to.number.)
-data$material = as.factor(data$Material..to.number.)
+data$turma = as.factor(data$Turma)
+data$material = as.factor(data$Material)
 data$mes = as.factor(data$Mês)
+data$turno = as.factor(data$Turno)
+
+data.materials$turma = as.factor(data.materials$Turma)
+data.materials$material = as.factor(data.materials$Material)
+data.materials$mes = as.factor(data.materials$Mês)
+data.materials$turno = as.factor(data.materials$Turno)
 
 #boxplot
 ggplot(data, aes(x=mes, y=VarConsumo_Eletrica)) + 
@@ -51,6 +57,18 @@ ggplot(data, aes(x=mes, y=VarConsumo_Quimica, fill = turma)) +
 ggplot(data, aes(x=mes, y=VarTempoProcesso, fill = turma)) + 
   geom_boxplot() 
 
+
+#boxplot3
+ggplot(data.materials, aes(x=mes, y=VarConsumo_Eletrica, fill = material)) + 
+  geom_boxplot() + facet_grid(cols = vars(turma), scales = "free_y")
+
+ggplot(data.materials, aes(x=mes, y=VarConsumo_Quimica, fill = material)) + 
+  geom_boxplot() + facet_grid(cols = vars(turma), rows=vars(material), scales = "free_y")
+
+ggplot(data.materials, aes(x=mes, y=VarTempoProcesso)) + 
+  geom_boxplot() + facet_grid(cols = vars(turma), rows=vars(material), scales = "free_y")
+
+
 #ridge
 #ggplot(data, aes(x = VarConsumo_Eletrica, y = mes, fill = turma)) + geom_density_ridges() +
 #  geom_density_ridges(scale = 10, size = 0.25, rel_min_height = 0.03) + theme(legend.position="bottom")
@@ -62,18 +80,21 @@ ggplot(data, aes(x=mes, y=VarTempoProcesso, fill = turma)) +
 #  geom_density_ridges() + theme(legend.position="bottom")
 
 #scatter
-ggplot(data, aes(x=VarConsumo_Quimica, y=VarConsumo_Eletrica, color=mes)) + geom_point() +  facet_grid(cols = vars(turma), scales = "free_y")
+ggplot(data, aes(x=VarConsumo_Quimica, y=VarConsumo_Eletrica, color=material)) + geom_point() +  facet_grid(cols = vars(turma), scales = "free_y")
 
 
-# Raw
-anv <- aov((VarTempoProcesso) ~ turma + mes, data = data)
+## Fatorial
+anv <- aov(log(VarConsumo_Quimica) ~  turno * material, data = data.materials)
 par(mfrow=c(2,2))
 plot(anv)
 summary(anv)
 
-
+# Multiple comparison
+tukey.test <- glht(anv, linfct = mcp(material = "Tukey"))
+tukey.test.CI <- confint(tukey.test, level = 0.95)
+par(mar = c(5, 8, 4, 2), las = 1)
 par(mfrow=c(1,1))
-qqPlot(anv$residuals)
+plot(tukey.test.CI, xlab = "Quimica")
 
 ## Quimica
 # Log transf
